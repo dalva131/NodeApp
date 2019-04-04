58df64d21cebd951bfdf213cd2e09d7dbed72732
Gustavo Gonzalez <gustavog525@gmail.com>
2019-04-04 00:51:23 -0400

    Node app with basic tests

---
+++ functions.test.js
@@ -0,0 +1,6 @@
+const functions= require('./functions')
+
+
+test('adds 2+2 to equal 4',()=>{
+	expect(functions.add(2,2)).toBe(4);
+})
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+var path=require('path')
+const app = express()
+const port = 3000
+
+
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname,'views'));
+
+app.get('/', function(req, res) {
+	res.render('index')
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
