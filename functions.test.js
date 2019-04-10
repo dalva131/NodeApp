d0e235ac5f2d12df53000b0a84935a7038032bda
dalva131 <42696206+dalva131@users.noreply.github.com>
2019-04-10 16:10:13 -0400

    Update functions.test.js
    Added for loop tests for each of the basic four functions. This would equate to 1,600 tests run, but since they're single operation, they should take no time to process.
    
    Also, the subtract function is misspelled in the original function file. Wonder what this would report if it can't find the function.

--- functions.test.js
+++ functions.test.js
@@ -1,7 +1,39 @@
 const functions= require('./functions')
 
 
-test('adds 2+2 to equal 4',()=>{
-	expect(functions.add(2,2)).toBe(4);
+test('addition on a range of numbers -10 to 10',()=>{
+	for (let a = -10; a <= 10; a++) {
+		for (let b = -10; b <= 10; b++) {
+			expect(functions.add(a,b)).toBe(a+b);
+		}
+	}
 });
 
+test('subtraction on a range of numbers -10 to 10',()=>{
+	for (let a = -10; a <= 10; a++) {
+		for (let b = -10; b <= 10; b++) {
+			expect(functions.subtract(a,b)).toBe(a-b);
+		}
+	}
+});
+
+test('multiplication on a range of numbers -10 to 10',()=>{
+	for (let a = -10; a <= 10; a++) {
+		for (let b = -10; b <= 10; b++) {
+			expect(functions.multiply(a,b)).toBe(a*b);
+		}
+	}
+});
+
+test('integer division on a range of numbers -10 to 10',()=>{
+	for (let a = -10; a <= 10; a++) {
+		for (let b = -10; b <= 10; b++) {
+			if (b === 0) {
+				expect(functions.divide(a,b)).toBeUndefined();
+			}
+			else {
+				expect(functions.divide(a,b)).toBe(a/b);
+			}
+		}
+	}
+});
